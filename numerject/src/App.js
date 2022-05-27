032028c9aa033b25237bdc2b158b9695b3344dbf
“s6204062630211” <s6204062630211@email.kmutnb.ac.th>
2022-05-27 19:14:47 +0700

    hi

--- numerject/src/App.js
+++ numerject/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
       <Switch>
         <Route exact path = '/Bisection' component ={Bisection}/>
         <Route exact path = '/FalsePosition' component ={FalsePosition}/>
-        <Route exact path = '/Punyawee' component ={Punyawee}/>
+        
         <Route exact path = '/OnePointIteration' component={OnePointIteration}/>
         <Route exact path = '/NewtonRaphson' component={NewtonRaphson}/>
         <Route exact path = '/Secant' component={Secant}/>
